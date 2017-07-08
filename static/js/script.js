cf9e3e490674ac675feff15d4988e8e54a3e32ad
kirkwiebe <kirk.wiebe@hudl.com>
2017-07-07 21:49:59 -0500

    ServerRequestAndResponse
    Adding basic requests and response for ‘age’ information.

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('dwarf').addEventListener('click', logger);
-  document.getElementById('elf').addEventListener('click', logger);
-  document.getElementById('halfling').addEventListener('click', logger);
-  document.getElementById('human').addEventListener('click', logger);
 
-  function logger(){
-    console.log(this.id);
+  document.getElementById('1').addEventListener('click', getData);
+  document.getElementById('2').addEventListener('click', getData);
+  document.getElementById('3').addEventListener('click', getData);
+  document.getElementById('4').addEventListener('click', getData);
+
+  function getData() {
+      // Instantiate XHR
+      var url = 'http://www.dnd5eapi.co/api/races/' + this.id;
+      var xhr;
+      if (window.XMLHttpRequest)
+          xhr = new XMLHttpRequest();
+      else if (window.ActiveXObject)
+          xhr = new ActiveXObject("Msxml2.XMLHTTP");
+      else
+          throw new Error("Ajax is not supported by your browser");
+
+      // server response handler
+      xhr.onreadystatechange = function () {
+          if (xhr.readyState < 4)
+              document.getElementById('metadata').innerHTML = "Loading...";
+          else if (xhr.readyState === 4) {
+              if (xhr.status == 200 && xhr.status < 300)
+              {
+                  var json = JSON.parse(xhr.responseText);
+                  console.log(json)
+                  document.getElementById('metadata').innerHTML = json.age;
+              }
+          }
+      }
+      // query server
+      xhr.open('GET', url);
+      xhr.send(null);
   }
 });
