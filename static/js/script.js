105d827fc0394be31f8a63eeaea9d82373f6cda1
kirkwiebe <kirk.wiebe@hudl.com>
2017-07-21 20:05:52 -0500

    AddSize
    Adds size and size description to the page.

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // add event listeners to race buttons
   for (i = 1; i < 5; i++)
   {
     document.getElementById(i).addEventListener('click', getData);
@@ -8,12 +9,11 @@ document.addEventListener('DOMContentLoaded', function () {
       var url = 'http://www.dnd5eapi.co/api/races/' + this.id;
       var xhr;
       if (window.XMLHttpRequest)
-          xhr = new XMLHttpRequest();
+        xhr = new XMLHttpRequest();
       else if (window.ActiveXObject)
-          xhr = new ActiveXObject("Msxml2.XMLHTTP");
+        xhr = new ActiveXObject("Msxml2.XMLHTTP");
       else
           throw new Error("Ajax is not supported by your browser");
-
       // server response handler
       xhr.onreadystatechange = function () {
           if (xhr.readyState < 4)
@@ -26,6 +26,8 @@ document.addEventListener('DOMContentLoaded', function () {
                   var bonuses = json.ability_bonuses;
                   document.getElementById('agedata').innerHTML = json.age;
                   document.getElementById('alignmentdata').innerHTML = json.alignment;
+                  document.getElementById('sizetitle').innerHTML = 'Size: ' + json.size;
+                  document.getElementById('sizedata').innerHTML = json.size_description;
                   for (i = 0; i < bonuses.length; i++)
                   {
                     document.getElementById('data.' + i).innerHTML = '+' + bonuses[i];
